3928f0782911abc5822578b74f388fb84f21773a
jerrel <jdjoneslmao@gmail.com>
2020-03-19 23:43:54 -0400

    removed unnecessary classes

--- app/src/main/java/Arms.java
+++
@@ -1,6 +0,0 @@
-public class Arms extends Exercise {
-    public Arms(String name, int avgDuration, String type, String primaryMuscleGroup, String secondaryMuscleGroup, long difficulty)
-    {
-        super(name, avgDuration, type, primaryMuscleGroup, secondaryMuscleGroup, difficulty);
-    }
-}
--- app/src/main/java/Legs.java
+++
@@ -1,6 +0,0 @@
-public class Legs extends Exercise {
-    public Legs(String name, int avgDuration, String type, String primaryMuscleGroup, String secondaryMuscleGroup, long difficulty)
-    {
-        super(name, avgDuration, type, primaryMuscleGroup, secondaryMuscleGroup, difficulty);
-    }
-}
